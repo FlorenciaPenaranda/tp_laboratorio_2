2e344ca357335dce388a75f1f020cc0cf0655e96
FlorenciaPenaranda <florenciaceciliap@gmail.com>
2019-11-18 23:48:48 -0300

    TP4 comienzo

---
+++ Peñaranda.Florencia.2D/Peñaranda.Florencia.2D/FrmPpal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peñaranda.Florencia._2D
+{
+    public partial class FrmPpal : Form
+    {
+        public FrmPpal()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
